c6486751a5edf9f0c68c9db5e964134af52c6854
smitzkar <smitzkar@gmail.com>
2025-09-03 16:53:04 +0200

    Update LICENSE, add skeleton files

---
+++ main.py
@@ -0,0 +1,14 @@
+""" 
+2025-09-03 A NEW PLAN  
+
+Create a fork of the project:  
+- actually embrace my silly abstractions (to be free from associations and baggage) 
+- use them in the final product!  
+-> no more `sensor_manager`, but `supervisor_owl`, `squirrel_1` instead of `message_carrier_1`, `crows` for `api calls` etc.
+=>  create a fantasy, where all the critters living in, on, and around the tree work together to give it a voice, to let the humans know that their actions are impacting them 
+
+- let Julia replace it with more serious names, via find-and-replace, if she wants to
+
+
+"""
+
